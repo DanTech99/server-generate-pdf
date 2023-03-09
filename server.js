977eb15ea217d1fd05b68eff880daaa76aaec7ac
DandevMS <xxdndkxx@gmail.com>
2023-03-09 00:53:36 -0500

    add change destructuring data, test bucle at the data, edit template html-pdf

--- server.js
+++ server.js
@@ -11,39 +11,48 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.post('/formulario', async (req, res) => {
-  const { date, tipo, firstFlo, finishFlo, observaciones } = req.body;
 
-  // Generar la plantilla HTML
-  const html = `
-    <html>
-      <head>
-        <style>
-          /* Estilos CSS para la plantilla */
-        </style>
-      </head>
-      <body>
-        <h1>Formulario</h1>
-        <p>Nombre: ${date}</p>
-        <p>Email: ${tipo}</p>
-        <p>Mensaje: ${firstFlo}</p>
-        <p>Mensaje: ${finishFlo}</p>
-        <p>Mensaje: ${observaciones}</p>
-      </body>
-    </html>
-  `;
+    const data = req.body;
+    console.log(data)
 
-  // Renderizar la plantilla HTML en un navegador virtual
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.setContent(html);
+    let html = `
+      <html>
+        <head>
+          <style>
+            /* Estilos CSS para la plantilla */
+          </style>
+        </head>
+        <body>
+          <h1>Formulario</h1>
+    `;
+    for (let i = 0; i < data.length; i++) {
+      // Se itera sobre cada objeto del array y se concatena su contenido en el HTML
+      html += `
+        <p>Fecha: ${data[i].fecha}</p>
+        <p>Tipo: ${data[i].tipo}</p>
+        <p>folio que inicia: ${data[i].firstFlo}</p>
+        <p>folio que termina: ${data[i].finishFlo}</p>
+        <p>Observaciones: ${data[i].observaciones}</p>
+      `;
+    }
+    html += `
+        </body>
+      </html>
+    `;
 
-  // Generar el PDF y enviarlo como respuesta
-  const pdf = await page.pdf({ format: 'A4' });
-  res.type('application/pdf');
-  res.send(pdf);
-
-  // Cerrar el navegador virtual
-  await browser.close();
+  
+    // Renderizar la plantilla HTML en un navegador virtual
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setContent(html);
+  
+    // Generar el PDF y enviarlo como respuesta
+    const pdf = await page.pdf({ format: 'A4' });
+    res.type('application/pdf');
+    res.send(pdf);
+  
+    // Cerrar el navegador virtual
+    await browser.close();
 });
 
 app.listen(port, () => console.log(`server listening on port:  ${port}`));
