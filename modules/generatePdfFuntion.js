d171b783497e39c24a2bc632de8a25d91dde91e3
DandevMs <xxdndkxx@gmail.com>
2023-03-24 01:02:02 -0500

    change args puppeter

--- modules/generatePdfFuntion.js
+++ modules/generatePdfFuntion.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
 async function generatePdfControl(html, res) {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
     const page = await browser.newPage();
     await page.setContent(html);
 
