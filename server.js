9322f5a5ccd681580dd0245da6ca4e2fe5335182
DandevMs <xxdndkxx@gmail.com>
2023-05-02 11:54:07 -0500

    Add CORS headers to all responses and update logo source in hojacontrol.handlebars

--- server.js
+++ server.js
@@ -75,12 +75,12 @@ app.set('view engine', 'handlebars');
 app.set('views', './views');
 
 // Agregar encabezados de CORS a todas las respuestas
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://server-generate-app.fly.dev');
+ app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://server-generate-pdf-production.up.railway.app/');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
-  });
+  }); 
 
 /**
  * ------------------------------------------------------------------------------
